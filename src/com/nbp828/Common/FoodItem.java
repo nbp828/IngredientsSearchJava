110f0eb6cad6898b86078205bbdfda139ed6356f
Neil Patel <nbp828@hotmail.com>
2019-12-15 23:35:02 -0500

    fooditem class

--- src/com/nbp828/Common/FoodItem.java
+++ src/com/nbp828/Common/FoodItem.java
@@ -2,10 +2,43 @@ package com.nbp828.Common;
 
 public class FoodItem {
 
+    String code;
+    String name;
+    Integer score;
+    Iterable<String> categories;
+    Iterable<String> ingredients;
 
 
-    public FoodItem()
+    public FoodItem(String code,
+                    String name,
+                    Integer score,
+                    Iterable<String> categories,
+                    Iterable<String> ingredients)
     {
+        this.code = code;
+        this.name = name;
+        this.score = score;
+        this.categories = categories;
+        this.ingredients = ingredients;
+    }
 
+    public String getCode() {
+        return this.code;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Integer getScore() {
+        return this.score;
+    }
+
+    public Iterable<String> getCategories() {
+        return categories;
+    }
+
+    public Iterable<String> getIngredients() {
+        return ingredients;
     }
 }
