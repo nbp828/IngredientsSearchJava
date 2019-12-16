149e73ddb337df92ef586435695506948b5555d3
Neil Patel <nbp828@hotmail.com>
2019-12-15 21:08:45 -0500

    Query Builder Template

--- src/com/nbp828/mongoDBWrapper/IngredientsDocumentClient.java
+++ src/com/nbp828/mongoDBWrapper/IngredientsDocumentClient.java
@@ -4,6 +4,7 @@ import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.ServerAddress;
 
+import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 
@@ -16,8 +17,24 @@ import static com.mongodb.client.model.Filters.*;
 import com.mongodb.client.result.DeleteResult;
 import static com.mongodb.client.model.Updates.*;
 import com.mongodb.client.result.UpdateResult;
+import org.bson.conversions.Bson;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class IngredientsDocumentClient {
+
+    MongoClient mongoClient;
+
+    public IngredientsDocumentClient()
+    {
+        this.mongoClient = new MongoClient();
+    }
+
+    public FindIterable<Document> getDocuments(Bson query)
+    {
+        MongoDatabase database = mongoClient.getDatabase("off");
+        MongoCollection<Document> collection = database.getCollection("product");
+        return collection.find(query);
+    }
 }
---
+++ src/com/nbp828/mongoDBWrapper/QueryBuilder.java
@@ -0,0 +1,19 @@
+package com.nbp828.mongoDBWrapper;
+
+import org.bson.BSON;
+
+public class QueryBuilder {
+
+    public static BSON getAllValidItemsQuery(){
+        return null;
+    }
+
+    public static BSON getOneFoodItemQuery(String code){
+        return null;
+    }
+
+    public static BSON getCategoryQuery(String category){
+        return null;
+    }
+
+}
