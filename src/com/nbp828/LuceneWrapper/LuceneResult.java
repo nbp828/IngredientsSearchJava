4466504162fc90f2dd0f8b5da334f6ee4e5afacc
Neil Patel <nbp828@hotmail.com>
2019-12-16 20:53:36 -0500

    results

--- src/com/nbp828/LuceneWrapper/LuceneResult.java
+++ src/com/nbp828/LuceneWrapper/LuceneResult.java
@@ -2,7 +2,33 @@ package com.nbp828.LuceneWrapper;
 
 public class LuceneResult {
 
-    public LuceneResult(String code, float score){
+    private String code;
+    private float score;
 
+    public LuceneResult(String code, float score){
+        this.code = code;
+        this.score = score;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public String resultToString(){
+        StringBuilder sb = new StringBuilder();
+        // code
+        sb.append("{code: \"");
+        sb.append(code);
+        sb.append("\", ");
+        // score
+        sb.append("score: ");
+        sb.append(score);
+        sb.append("}");
+
+        return sb.toString();
     }
 }
--- src/com/nbp828/LuceneWrapper/LuceneSearcher.java
+++ src/com/nbp828/LuceneWrapper/LuceneSearcher.java
@@ -22,35 +22,32 @@ import java.util.*;
 
 public class LuceneSearcher {
 
-    public void Search(String indexPath, String queryString) {
+    public ArrayList<LuceneResult> Search(String indexPath, String queryString)
+            throws Exception
+    {
+        IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
+        IndexSearcher searcher = new IndexSearcher(reader);
+        searcher.setSimilarity(new BM25Similarity());
+        Analyzer analyzer = new StandardAnalyzer();
+        String field = "contents";
+        QueryParser parser = new QueryParser(field, analyzer);
 
-        try {
-            IndexReader reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexPath)));
-            IndexSearcher searcher = new IndexSearcher(reader);
-            searcher.setSimilarity(new BM25Similarity());
-            Analyzer analyzer = new StandardAnalyzer();
-            String field = "contents";
-            QueryParser parser = new QueryParser(field, analyzer);
-
-            queryString = queryString.trim();
-            if (queryString.length() == 0) {
-                throw new IllegalArgumentException("bad query string");
-            }
-
-            Query query = parser.parse(queryString);
-            System.out.println("Searching for: " + query.toString(field));
-
-            this.doPagingSearch(searcher, query, 10);
-
-        } catch (Exception e) {
-            System.out.println(e.toString());
+        queryString = queryString.trim();
+        if (queryString.length() == 0) {
+            throw new IllegalArgumentException("bad query string");
         }
 
-        // sb.append(System.getProperty("line.separator"));
+        Query query = parser.parse(queryString);
+        System.out.println("Searching for: " + query.toString(field));
+
+        ArrayList<LuceneResult> results = this.doPagingSearch(searcher, query, 10);
+        return results;
     }
 
-    private void doPagingSearch(IndexSearcher searcher, Query query, int hitsPerPage) throws IOException {
-
+    private ArrayList<LuceneResult> doPagingSearch(IndexSearcher searcher, Query query, int hitsPerPage)
+            throws IOException
+    {
+        ArrayList<LuceneResult> luceneResults = new ArrayList<>();
         TopDocs results = searcher.search(query, hitsPerPage);
         ScoreDoc[] hits = results.scoreDocs;
         for (ScoreDoc hit : hits) {
@@ -58,24 +55,11 @@ public class LuceneSearcher {
             String path = doc.get("path");
             String[] dirs = path.split("/");
             String code = dirs[dirs.length - 1];
-
-
-
-            // System.out.println(resultToString(code, hit.score));
+            LuceneResult result = new LuceneResult(code, hit.score);
+            luceneResults.add(result);
         }
+        return luceneResults;
     }
 
-    private String resultToString(String code, float score){
-        StringBuilder sb = new StringBuilder();
-        // code
-        sb.append("{code: \"");
-        sb.append(code);
-        sb.append("\", ");
-        // score
-        sb.append("score: ");
-        sb.append(score);
-        sb.append("}");
 
-        return sb.toString();
-    }
 }
